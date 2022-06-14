b19f31c93f909f44b4920ac07cfc6331f26f5bc9
Toffe PC hjemme <toffehelstad@gmail.com>
2022-06-15 01:22:37 +0200

    FEAT: MovementScripts

---
+++ Assets/Scripts/Player Specific/ThirdPersonMovement.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    [Header("Gravity")]
+    [Tooltip("Adds gravity. Default is: 1")]
+    public float gravityMultiplier = 1f;                 //Sets a gravity multiplier
+    [Tooltip("Drag GroundCheck object here")]
+    public Transform groundCheck;                       //Reference to object that checks if the ground is under the player
+    [Tooltip("Select layer that is set as ground level")]
+    public LayerMask groundLayer;                       //Reference to which layer is "ground"
+    [Tooltip("Is the player on the ground?")]
+    public bool isGrounded;                             //bool that confirms if the player is grounded
+
+    [Header("Speed")]
+    [Tooltip("Sets walkspeed. Default is: 6")]
+    public float walkSpeed = 6f;                                             //Variable for walkSpeed
+    [Tooltip("Sets runspeed. Should generally be 2-3 above Walkspeed")]
+    public float runSpeed = 8f;                                              //Variable for runSpeed
+
+    private float gravityConstant = -9.71f;             //Sets a constant for gravity calculations
+    private CharacterController cc;                     //Reference to Character Controller component on object
+    private Vector3 velocity;                           //Variables for velocity
+
+    private Quaternion rotation;
+
+    [HideInInspector]
+    public Vector3 movementDir;
+
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();       //Fetches character controller component
+    }
+
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, 0.4f, groundLayer); //isGrounded is decided through a spherecast
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2;
+        }
+        else
+        {
+            velocity.y += gravityConstant * gravityMultiplier * Time.deltaTime;
+        }
+
+        cc.Move(velocity * Time.deltaTime);
+
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+        movementDir = ((transform.right * x) + (transform.forward * z)).normalized;
+
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Run();
+        }
+        else
+        {
+            Move();
+        }
+    }
+
+    private void Move()
+    {
+        Vector3 motion = movementDir * walkSpeed * Time.deltaTime;
+        cc.Move(motion);
+        
+    }
+
+    private void Run()
+    {
+        Vector3 motion = movementDir * runSpeed * Time.deltaTime;
+        cc.Move(motion);
+    }
+}
